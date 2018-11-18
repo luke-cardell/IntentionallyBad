b4d3ebffddc83cf5183163ea613426f54580ae45
Luke Cardell <lucas.cardell@digipen.edu>
2018-11-17 17:26:53 -0800

    Added README and MDB

---
+++ MDB/MDB.cpp
@@ -0,0 +1,49 @@
+/* MDB */
+#include <string>
+#include <cstdlib>
+
+#define _ {
+#define __ }
+#define ___ system
+#define ____ char
+#define _____ std::string
+#define ______ if
+#define _______ for
+#define ________(o) return o
+#define __________ void
+#define ___p___ int
+#define ___p_ double
+#define _p_____ 5
+#define __p___ main
+
+____ _p = 0x1A;
+
+__________ _________(_____ _p_,____ __p_)                                     _
+;______(__p_>=_p)________()
+;_______(____ __p__=0x41
+;(__p__)<=0x5A
+;__p__=(__p__+0b1))                                                           _
+;____ b[0b111]=                                                               _
+0x6D,0x6B,0x64,0x69,0x72,0x20,0x00                                            __
+;_____ _p__(b)
+;_p__+=_p_+____(0x2F)
+;_p__+=__p__
+;___(_p__.c_str())
+;_p__=_p_
+;_p__+=____(0x2F)
+;_p__+=__p__
+;_________(_p__,(__p_+0b1))
+;                                                                             __
+                                                                              __
+
+___p___ __p___(__________)                                                    _
+____ _p_[0b10]=                                                               _
+0x2E,0x00                                                                     __
+;_____ __p_(_p_)
+;_________(__p_,0b11111111^0xFF)
+;________(0)
+;                                                                             __
+
+/* Authored by Luke Cardell
+** Open source under Gnu Public License
+*/
---
+++ README.txt
@@ -0,0 +1,20 @@
+Intentionally Bad
+Written by Luke Cardell
+ReadMe last updated 11/17/2018
+
+This is a repository for intentionally bad code,
+be it in implementation, style, or function. It is
+my firm belief that poorly written code serves a
+purpose more fundamental to the nature of learning
+to program than clean, well-structured, and
+consistent code. It is the poorly written code
+that, through experience and redesign, we learn
+the most from.
+
+None of the code in this repository is intended to
+be used in a malicious fashion. Some of it may do
+stupid or useless things, but none of it is
+intentionally harmful.
+
+All code in this repository is open source under
+the Gnu Public License.
